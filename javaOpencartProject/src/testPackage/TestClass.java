7e1fa69257e134cf7da03ab9b1026574f2e9b3b7
vaibhav-deshmukh01 <127335007+vaibhav-deshmukh01@users.noreply.github.com>
2023-03-09 03:47:19 +0530

    First commit

---
+++ javaOpencartProject/src/testPackage/TestClass.java
@@ -0,0 +1,13 @@
+package testPackage;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test base class");
+		System.out.println("pom.xml");
+		System.out.println("utili");
+
+	}
+
+}
