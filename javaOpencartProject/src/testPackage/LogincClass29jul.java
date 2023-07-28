bfb4c561b72e64c14c883023e8b0e0c88331a8c5
vaibhav-deshmukh01 <127349914+vaibhav-deshmukh007@users.noreply.github.com>
2023-07-29 02:09:54 +0530

    Create LogincClass29jul.java
    jjjjjjjjjjjj29jul

---
+++ javaOpencartProject/src/testPackage/LogincClass29jul.java
@@ -0,0 +1,15 @@
+package testPackage;
+
+public class LogincClass29jul {
+
+	public static void main(String[] args) {
+
+		System.out.println("nnnnn");
+
+		System.out.println("KGhhhhn");
+
+		System.out.println("xyzbvvv");
+
+	}
+
+}
